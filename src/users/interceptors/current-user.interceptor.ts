4fe422ac4cdd11bd88a414efdc1779051d0821ac
JoseIgnacioTapia <josignaciot@gmail.com>
2025-04-22 17:24:17 -0300

    Connect an Interceptor to Dependency Injection

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -6,12 +6,13 @@ import {
 } from '@nestjs/common';
 import { UsersService } from '../users.service';
 
+@Injectable()
 export class CurrentUserInterceptor implements NestInterceptor {
   constructor(private usersService: UsersService) {}
 
   async intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+    const { userId } = request.session || {};
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
