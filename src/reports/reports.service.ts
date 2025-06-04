2cd8b496522d689c21d66abdc39b47f10b9f795b
JoseIgnacioTapia <josignaciot@gmail.com>
2025-06-03 21:02:00 -0300

    Test the Estimate Logic

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -16,10 +16,10 @@ export class ReportsService {
       .select('AVG(price)', 'price')
       .where('make = :make', { make })
       .andWhere('model = :model', { model })
-      .andWhere('lng - :lg BETWEEN -5 AND 5', { lng })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
       .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
       .andWhere('year - :year BETWEEN -3 AND 3', { year })
-      .orderBy('ABS(milage - :mileage)', 'DESC')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
       .setParameters({ mileage })
       .limit(3)
       .getRawOne();
